e7bd4359c71f155416904e075e3a6ac7b123695b
AnnaE <annaefitzgerald@gmail.com>
2021-09-01 08:08:12 -0600

    react/jsx project setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello Mexico City!</h1>
+                <h2>CDMX To Do</h2>
+                    <ul>
+                        <li>Museo Soumaya</li>
+                        <li>Mercado Roma</li>
+                        <li>El Moro Churerria</li>
+                    </ul>      
+            </header>
+        </div>
+    );
+}
+
+export default App;
